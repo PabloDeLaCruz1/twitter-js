0aeafb1aebf736c63215e688955bb3d90a4ec87c
Pablo De La Cruz <pdelac@ramapo.edu>
2020-12-01 01:11:37 -0500

    middeware testing

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require("express")
+const chalk = require("chalk")
+const morgan = require('morgan')
 
 const app = express()
 const PORT = 3000
 
-app.use(express.static('public'))
-app.use(function (req, res, next) {
-    console.log('Time:', Date.now())
-    next()
-  })
-  
+//middleware
+// app.use(express.static('public'))
+// app.use(function (req, res, next) {
+//     console.log('Time:', Date.now())
+//     // console.log( res.status(201));
+//     next()
+// })
+
+// //route specific middleware
+// app.use("/special",function (req, res, next) {
+//     console.log(chalk.yellow("this is special"))
+//     next()
+// })
+
+app.use(morgan('combined'))
+
+
 app.get('/', (req, res, next) => {
     res.send("Hello Twitter World!")
     next();
@@ -18,5 +31,5 @@ app.get('/news', (req, res, next) => {
     next();
 })
 app.listen(PORT, () => {
-    console.log("Server is listening on port 3000");
+    console.log(chalk.blue("Server is listening on port 3000"));
 })
